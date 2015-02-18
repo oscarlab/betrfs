b91b4891a5975b22790aa4c460671155e04f4d83
ftfs <ftfs@beaker0.oscar.cs.stonybrook.edu>
2015-02-18 15:13:44 -0500

    Cleanup for release

--- tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tmux new-session -d -s toku -n main
-tmux new-window -t toku:1 -n grep
-tmux new-window -t toku:2 -n compile
-tmux new-window -t toku:3 -n debug
-tmux new-window -t toku:4 -n paper
-tmux select-window -t toku:0
