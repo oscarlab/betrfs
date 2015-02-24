2f011c2357cf3bf4f6353f1bf9d658e30fb610b3
wjannen <wjannen@cs.stonybrook.edu>
2015-02-23 19:08:26 -0500

    update cmake-ft.sh script
    this script provides a default set of options to pass to
    cmake when building the ft-index code.
    
    two-stage build:
              stage 1, build ft-index
              stage 2, build filesystem/ module, which pulls in
                       relevant ft-index code as a binary blob
    
    stage 2 assumes ft-index is built in the build/ directory

--- cmake-ft.sh
+++ cmake-ft.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+## This script will compile the ft-index code using cmake.
+## If you are not an expert on cmake, here is some advice:
+##   do an out-of-source build.
+##
+##  mkdir build
+##  cp cmake-ft.sh build/.
+##  cd build
+##  ./cmake-ft.sh
+##
+
 set -x
 
 TYPE=RELEASE
+#TYPE=RELWITHDEBINFO
 #TYPE=DEBUG
 CC=gcc-4.7 CXX=g++-4.7 cmake \
      -D CMAKE_BUILD_TYPE=$TYPE \
