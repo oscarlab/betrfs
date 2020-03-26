7e43db4ba5bef070a921c1de3af3f132abd047e1
Don Porter <porter@cs.unc.edu>
2020-03-26 09:48:00 -0400

    Update the fs-info script

--- benchmarks/fs-info.sh
+++ benchmarks/fs-info.sh
@@ -2,12 +2,22 @@
 #  and the root fs is installed on the HDD on /dev/sdb
 
 #sb_dev=/dev/sdb3
-sb_dev=/dev/sdb1
+sb_dev=/dev/sdb
 dummy_file=dummy.dev
 dummy_dev=/dev/loop0
 mntpnt=/mnt/benchmark
-southbound_module=/home/betrfs/ft-index/ftfs/ftfs.ko
-module=/home/betrfs/ft-index/filesystem/ftfs.ko
-repo=ft-index
+defrepo=betrfs-private
+REPO=${REPO:-betrfs-private}
+user_owner=betrfs:betrfs
+#repo=master/betrfs-private
 clone_repo=linux
 circle_size=128
+CUR_MNTPNT=${CUR_MNTPNT:-/home/betrfs}
+FT_HOMEDIR=$CUR_MNTPNT/$REPO
+southbound_module=$FT_HOMEDIR/ftfs/ftfs.ko
+module=$FT_HOMEDIR/filesystem/ftfs.ko
+allfs=(ext4 ftfs)
+alltest=(ext4 master)
+use_sfs=false
+resultdir=$FT_HOMEDIR/benchmarks/results
+test_num=3
