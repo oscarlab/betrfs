19a9b307525faefd31865be99dac84724be60890
Don Porter <porter@cs.unc.edu>
2020-06-09 14:53:04 -0400

    Try again to get the kernel built in Jenkins

--- Jenkinsfiles/build-bench.sh
+++ Jenkinsfiles/build-bench.sh
@@ -4,7 +4,7 @@ touch console.out
 sudo chown libvirt-qemu console.out
 
 vagrant up > /dev/null
-vagrant ssh -c "set -e; cd /oscar/betrfs; wget https://auku.cs.unc.edu/depot/linux-source-3.11.10-ftfs_20180618.00_all.deb; sudo dpkg -i linux-source-3.11.10-ftfs_20180618.00_all.deb; tar -xf /usr/src/linux-source-3.11.10-ftfs.tar.bz2; mv linux-source-3.11.10-ftfs linux-3.11.10; cd linux-3.11.10; cp /boot/config-$(uname -r) .config; make oldconfig; make -j 4; cd ..; mkdir -p build; cp qemu-utils/cmake-ft.sh build/; cd build; sed -i 's/DEBUG/RELEASE/g' cmake-ft.sh; ./cmake-ft.sh; cd ../filesystem; make"
+vagrant ssh -c "set -e; cd /oscar/betrfs; wget https://auku.cs.unc.edu/depot/linux-source-3.11.10-ftfs_20180618.00_all.deb; sudo dpkg -i linux-source-3.11.10-ftfs_20180618.00_all.deb; tar -xf /usr/src/linux-source-3.11.10-ftfs.tar.bz2; mv linux-source-3.11.10-ftfs linux-3.11.10; cd linux-3.11.10; cp ../qemu-utils/jenkins.config.3.11.10 .config; make oldconfig; make -j 4; cd ..; mkdir -p build; cp qemu-utils/cmake-ft.sh build/; cd build; sed -i 's/DEBUG/RELEASE/g' cmake-ft.sh; ./cmake-ft.sh; cd ../filesystem; make"
 result=$?
 
 exit ${result}
