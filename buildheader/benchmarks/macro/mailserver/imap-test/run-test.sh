50580449072d894e2e99ba3b466bf36de0c1f727
Erica Fu <ericafu@renci.org>
2021-01-29 13:40:38 -0500

    remove moved files

--- buildheader/benchmarks/macro/mailserver/imap-test/run-test.sh
+++
@@ -1,8 +0,0 @@
-cd ~/ft-index/benchmarks/
-sudo ./cleanup-fs.sh
-sudo ./setup-ftfs.sh
-cd -
-sudo cp -r /home/ftfstest/ftfstest/ /mnt/benchmark/
-sudo chown -R ftfstest:ftfstest /mnt/benchmark/ftfstest/
-sudo dovecot
-./imap-punish.py
