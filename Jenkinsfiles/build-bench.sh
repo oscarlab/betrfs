a45bf39d78988859457264707858b8e08f602490
Sagar Patel <sagarmp@cs.unc.edu>
2022-04-07 10:24:25 -0400

    drop kernel download and install
    The correct kernel must already be running for this pipeline to work, so
    downloading and installing the 3.11 kernel should not be necessary.

--- Jenkinsfiles/build-bench.sh
+++ Jenkinsfiles/build-bench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vagrant ssh -c "set -e; cd /oscar/betrfs; wget https://auku.cs.unc.edu/depot/linux-source-3.11.10-ftfs_20180618.00_all.deb; sudo dpkg -i linux-source-3.11.10-ftfs_20180618.00_all.deb; mkdir -p build; cp qemu-utils/cmake-ft.sh build/; cd build; sed -i 's/DEBUG/RELEASE/g' cmake-ft.sh; ./cmake-ft.sh; cd ../filesystem; make"
+vagrant ssh -c "set -e; cd /oscar/betrfs; mkdir -p build; cp qemu-utils/cmake-ft.sh build/; cd build; sed -i 's/DEBUG/RELEASE/g' cmake-ft.sh; ./cmake-ft.sh; cd ../filesystem; make"
 result=$?
 
 exit ${result}
